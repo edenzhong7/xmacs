d1e2706a9bc5a28384bf38197a3a68111900d91b
edenzhong <edenzhong@futunn.com>
2025-01-23 11:02:09 +0800

    update lsp-bridge module

--- doom/modules/x/lsp-bridge/config.el
+++ doom/modules/x/lsp-bridge/config.el
@@ -12,5 +12,12 @@
         "j"             #'lsp-bridge-find-def)
   (require 'yasnippet)
   (yas-global-mode 1)
+  (setq lsp-bridge-enable-log nil)
   (setq acm-enable-codeium t)
   (global-lsp-bridge-mode))
+
+(use-package! dape
+  :config
+  (setq dap-auto-configure-features '(sessions locals controls tooltip))  ;; 启用调试控制
+  (setq dap-debug-template-options '( :cwd nil ))  ;; 设置工作目录
+  )
